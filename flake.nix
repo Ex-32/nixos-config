85b58767ad88d28327ab91b5684c2fb258ac5a47
Ex-32 <jenna@fligor.net>
2023-09-18 11:37:54 -0500

    fixed whitespace

--- flake.nix
+++ flake.nix
@@ -27,19 +27,19 @@
         ];
       };
       "nixbook" = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            ./hardware/nixbook.nix
-            ./system.nix
-            home-manager.nixosModules.home-manager
-            {
-                home-manager.useGlobalPkgs = true;
-                home-manager.useUserPackages = true;
-                home-manager.users.jenna = import ./home.nix;
+        system = "x86_64-linux";
+        modules = [
+          ./hardware/nixbook.nix
+          ./system.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.jenna = import ./home.nix;
             # Optionally, use home-manager.extraSpecialArgs to pass
             # arguments to home.nix      
-            }
-          ];
+          }
+        ];
       };
     };
   };
