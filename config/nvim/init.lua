9fdd35b5c9bd502b91167977995a5652859a998b
Ex-32 <jenna@fligor.net>
2023-12-18 12:17:46 -0600

    migrated all statically defined keybinds to mapping table

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -314,56 +314,37 @@ require('lazy').setup({
 
 -- [[ Setting options ]]
 
-vim.o.hlsearch = true          -- highlight on search
+vim.o.hlsearch = true           -- highlight on search
 vim.wo.number = true            -- line numbers by default
 vim.o.mouse = 'a'               -- enable mouse mode
 vim.o.clipboard = 'unnamedplus' -- sync clipboard between OS and neovim
 vim.o.breakindent = true        -- enable break indent
 vim.o.undofile = true           -- save undo history
-vim.o.ignorecase = true         -- Case-insensitive searching UNLESS \C or capital in search
-vim.o.smartcase = true          -- ^
 vim.wo.signcolumn = 'yes'       -- enable gutter with stuff like git flags
+vim.o.spelllang = "en_us,cjk"   -- set spellcheck language
+vim.o.spellsuggest = "best,5"   -- set spellcheck suggestion options
+vim.o.spell = true              -- enable spellcheck
+vim.o.colorcolumn = "80"        -- add ruler at 80 columns 
+vim.o.termguicolors = true      -- enable 255 terminal colors
+vim.o.wrap = true               -- soft wrap lines that are too long to display
+vim.o.linebreak = true          -- try to break long lines at word boundaries
 
+-- Case-insensitive searching UNLESS \C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
 -- Decrease update time
 vim.o.updatetime = 250
 vim.o.timeoutlen = 300
-
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
 
-vim.o.termguicolors = true
-
-vim.o.colorcolumn = "80"
-
-vim.o.wrap = true
-vim.o.linebreak = true
-
-vim.o.spelllang = "en_us,cjk"
-vim.o.spellsuggest = "best,5"
-vim.o.spell = true
+-- disable spellcheck and line numbers on terminal windows
 vim.api.nvim_create_autocmd("TermOpen", {
   callback = function()
     vim.opt_local.spell = false
     vim.opt_local.number = false
   end
 })
-
-
--- [[ Basic Keymaps ]]
-
--- Keymaps for better default experience
--- See `:help vim.keymap.set()`
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-
--- Remap for dealing with word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-
--- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
@@ -392,70 +373,6 @@ require('telescope').setup {
 -- Enable telescope fzf native, if installed
 pcall(require('telescope').load_extension, 'fzf')
 
--- Telescope live_grep in git root
--- Function to find the git root directory based on the current buffer's path
-local function find_git_root()
-  -- Use the current buffer's path as the starting point for the git search
-  local current_file = vim.api.nvim_buf_get_name(0)
-  local current_dir
-  local cwd = vim.fn.getcwd()
-  -- If the buffer is not associated with a file, return nil
-  if current_file == '' then
-    current_dir = cwd
-  else
-    -- Extract the directory from the current file's path
-    current_dir = vim.fn.fnamemodify(current_file, ':h')
-  end
-
-  -- Find the Git root directory from the current file's path
-  local git_root = vim.fn.systemlist('git -C ' .. vim.fn.escape(current_dir, ' ') .. ' rev-parse --show-toplevel')[1]
-  if vim.v.shell_error ~= 0 then
-    print 'Not a git repository. Searching on current working directory'
-    return cwd
-  end
-  return git_root
-end
-
--- TODO: convert everything from here to the Treesitter section to the 
--- declarative mapping table
-
--- Custom live_grep function to search in git root
-local function live_grep_git_root()
-  local git_root = find_git_root()
-  if git_root then
-    require('telescope.builtin').live_grep {
-      search_dirs = { git_root },
-    }
-  end
-end
-
-vim.api.nvim_create_user_command('LiveGrepGitRoot', live_grep_git_root, {})
-
--- See `:help telescope.builtin`
-vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
-vim.keymap.set('n', '<leader>fc', function()
-  -- You can pass additional configuration to telescope to change theme, layout, etc.
-  require('telescope.builtin').current_buffer_fuzzy_find(--[[require('telescope.themes').get_dropdown {
-    winblend = 10,
-    previewer = false,
-  }--]])
-end, { desc = '[F]ind in [C]urrent buffer' })
-vim.keymap.set('n', '<leader>f/', function()
-  require('telescope.builtin').live_grep {
-    grep_open_files = true,
-    prompt_title = 'Live Grep in Open Files',
-  }
-end , { desc = '[F]ind [/] in Open Files' })
-vim.keymap.set('n', '<leader>fs', require('telescope.builtin').builtin, { desc = '[F]ind [S]elect Telescope' })
-vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Find [G]it [F]iles' })
-vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, { desc = '[F]ind [F]iles' })
-vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags, { desc = '[F]ind [H]elp' })
-vim.keymap.set('n', '<leader>fw', require('telescope.builtin').grep_string, { desc = '[F]ind current [W]ord' })
-vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep, { desc = '[F]ind by [G]rep' })
-vim.keymap.set('n', '<leader>fG', ':LiveGrepGitRoot<cr>', { desc = '[F]ind by [G]rep on Git Root' })
-vim.keymap.set('n', '<leader>fd', require('telescope.builtin').diagnostics, { desc = '[F]ind in [D]iagnostics' })
-vim.keymap.set('n', '<leader>fr', require('telescope.builtin').resume, { desc = '[F]ind [R]esume' })
 
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
@@ -635,6 +552,16 @@ require('which-key').register({
   ['<leader>h'] = { 'Git [H]unk' },
 }, { mode = 'v' })
 
+-- Setup neovim lua configuration
+require("neodev").setup({
+  override = function(root_dir, library)
+    if root_dir:find("/etc/nixos", 1, true) == 1 then
+      library.enabled = true
+      library.plugins = true
+    end
+  end,
+})
+
 local lspconfig = require('lspconfig')
 local servers = {
   clangd = {
@@ -672,7 +599,6 @@ local servers = {
         workspace = { checkThirdParty = false },
         telemetry = { enable = false },
         -- diagnostics = {
-        --   -- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
         --   disable = { 'missing-fields' },
         -- },
       },
@@ -694,16 +620,6 @@ for key, value in pairs(servers) do
     root_dir = (value or {}).root_dir,
   })
 end
-
--- Setup neovim lua configuration
-require("neodev").setup({
-  override = function(root_dir, library)
-    if root_dir:find("/etc/nixos", 1, true) == 1 then
-      library.enabled = true
-      library.plugins = true
-    end
-  end,
-})
 
 -- [[ Configure nvim-cmp ]]
 -- See `:help cmp`
@@ -757,6 +673,30 @@ cmp.setup {
   },
 }
 
+-- Telescope live_grep in git root
+-- Function to find the git root directory based on the current buffer's path
+local function helper_find_git_root()
+  -- Use the current buffer's path as the starting point for the git search
+  local current_file = vim.api.nvim_buf_get_name(0)
+  local current_dir
+  local cwd = vim.fn.getcwd()
+  -- If the buffer is not associated with a file, return nil
+  if current_file == '' then
+    current_dir = cwd
+  else
+    -- Extract the directory from the current file's path
+    current_dir = vim.fn.fnamemodify(current_file, ':h')
+  end
+
+  -- Find the Git root directory from the current file's path
+  local git_root = vim.fn.systemlist('git -C ' .. vim.fn.escape(current_dir, ' ') .. ' rev-parse --show-toplevel')[1]
+  if vim.v.shell_error ~= 0 then
+    print 'Not a git repository. Searching on current working directory'
+    return cwd
+  end
+  return git_root
+end
+
 local mappings = {
   i = {
     -- go to  beginning and end
@@ -770,6 +710,7 @@ local mappings = {
     ["<C-k>"] = { "<Up>", "Move up" },
   },
   n = {
+    ["<space>"] = { "<Nop>", nil, { silent = true } },
     ["<leader>s"] = { "<cmd> set spell! <CR>", "Toggle Spellcheck" },
     ["<Esc>"] = { "<cmd> noh <CR>", "Clear highlights" },
     ["<C-s>"] = { "<cmd> w <CR>", "Save file" },
@@ -782,18 +723,60 @@ local mappings = {
       "Toggle comment",
     },
 
+    -- Diagnostic keymaps
+    ["[d"] = { vim.diagnostic.goto_prev, "Go to previous diagnostic message" },
+    ["]d"] = { vim.diagnostic.goto_next, "Go to next diagnostic message" },
+    ["<leader>e"] = { vim.diagnostic.open_float, "Open floating diagnostic message" },
+    ["<leader>q"] = { vim.diagnostic.setloclist, "Open diagnostics list" },
+
+    -- telescope mappings
+    ["<leader>ft"] = { "<cmd> TodoTelescope <CR>", "[F]ind [T]ODO tags" },
+    ["<leader>?"] = { require('telescope.builtin').oldfiles, "[?] Find recently opened files" },
+    ["<leader><space>"] = { require('telescope.builtin').buffers, "[ ] Find existing buffers" },
+    ["<leader>fc"] = { require("telescope.builtin").current_buffer_fuzzy_find, "[F]ind in [C]urrent buffer" },
+    ["<leader>fs"] = { require('telescope.builtin').builtin, "[F]ind [S]elect Telescope" },
+    ["<leader>gf"] = { require('telescope.builtin').git_files, "Find [G]it [F]iles" },
+    ["<leader>ff"] = { require('telescope.builtin').find_files, "[F]ind [F]iles" },
+    ["<leader>fh"] = { require('telescope.builtin').help_tags, "[F]ind [H]elp" },
+    ["<leader>fw"] = { require('telescope.builtin').grep_string, "[F]ind current [W]ord" },
+    ["<leader>fg"] = { require('telescope.builtin').live_grep, "[F]ind by [G]rep" },
+    ["<leader>fd"] = { require('telescope.builtin').diagnostics, "[F]ind in [D]iagnostics" },
+    ["<leader>fr"] = { require('telescope.builtin').resume, "[F]ind [R]esume" },
+    ["<leader>f/"] = {
+      function()
+        require('telescope.builtin').live_grep {
+          grep_open_files = true,
+          prompt_title = 'Live Grep in Open Files',
+        }
+      end,
+      "[F]ind [/] in open files",
+    },
+    ["<leader>fG"] = {
+      function()
+        local git_root = helper_find_git_root()
+        if git_root then
+          require('telescope.builtin').live_grep {
+            search_dirs = { git_root },
+          }
+        end
+      end,
+      "[F]ind by [G]rep on Git Root",
+    },
+
     -- Allow moving the cursor through wrapped lines with j, k, <Up> and <Down>
     -- http://www.reddit.com/r/vim/comments/2k4cbr/problem_with_gj_and_gk/
     -- empty mode is same as using <cmd> :map
     -- also don't use g[j|k] when in operator pending mode, so it doesn't alter d, y or c behaviour
-    ["j"] = { 'v:count || mode(1)[0:1] == "no" ? "j" : "gj"', "Move down", { expr = true } },
-    ["k"] = { 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"', "Move up", { expr = true } },
-    ["<Up>"] = { 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"', "Move up", { expr = true } },
-    ["<Down>"] = { 'v:count || mode(1)[0:1] == "no" ? "j" : "gj"', "Move down", { expr = true } },
+    ["j"] = { 'v:count || mode(1)[0:1] == "no" ? "j" : "gj"', "Move down", { expr = true, silent = true } },
+    ["k"] = { 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"', "Move up", { expr = true, silent = true } },
+    ["<Up>"] = { 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"', "Move up", { expr = true, silent = true } },
+    ["<Down>"] = { 'v:count || mode(1)[0:1] == "no" ? "j" : "gj"', "Move down", { expr = true, silent = true } },
   },
   v = {
-    ["<Up>"] = { 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"', "Move up", { expr = true } },
-    ["<Down>"] = { 'v:count || mode(1)[0:1] == "no" ? "j" : "gj"', "Move down", { expr = true } },
+    ["<space>"] = { "<Nop>", nil, { silent = true } },
+
+    ["<Up>"] = { 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"', "Move up", { expr = true, silent = true } },
+    ["<Down>"] = { 'v:count || mode(1)[0:1] == "no" ? "j" : "gj"', "Move down", { expr = true, silent = true } },
 
     ["<leader>/"] = {
       "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
@@ -801,8 +784,8 @@ local mappings = {
     },
   },
   x = {
-    ["j"] = { 'v:count || mode(1)[0:1] == "no" ? "j" : "gj"', "Move down", { expr = true } },
-    ["k"] = { 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"', "Move up", { expr = true } },
+    ["j"] = { 'v:count || mode(1)[0:1] == "no" ? "j" : "gj"', "Move down", { expr = true, silent = true } },
+    ["k"] = { 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"', "Move up", { expr = true, silent = true } },
     -- Don't copy the replaced text after pasting in visual mode
     -- https://vim.fandom.com/wiki/Replace_a_word_with_yanked_text#Alternative_mapping_for_paste
     ["p"] = { 'p:let @+=@0<CR>:let @"=@0<CR>', "Dont copy replaced text", { silent = true } },
