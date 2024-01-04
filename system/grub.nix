6c426f0bf9e625f185c69bf114f02061f41a2cd5
Ex-32 <jenna@fligor.net>
2024-01-04 17:32:34 -0600

    switched catppuccin grub theme to github version tag

--- system/grub.nix
+++ system/grub.nix
@@ -18,11 +18,11 @@
       path = "src/catppuccin-mocha-grub-theme";
     in pkgs.stdenv.mkDerivation rec {
       pname = "catppuccin-mocha-grub";
-      version = "803c5df0e83aba61668777bb96d90ab8f6847106";
+      version = "1.0.0";
       src = pkgs.fetchFromGitHub {
         owner = "catppuccin";
         repo = "grub";
-        rev = "${version}";
+        rev = "v${version}";
         hash = "sha256-/bSolCta8GCZ4lP0u5NVqYQ9Y3ZooYCNdTwORNvR7M0=";
       };
       nativeBuildInputs = with pkgs; [
