e6754460c55cdecad336a734515ef4430c43ad71
Ex-32 <jenna@fligor.net>
2024-02-05 22:05:55 -0600

    fixed issue where sway volume-up button didn't work

--- user/sway.nix
+++ user/sway.nix
@@ -154,7 +154,7 @@
         "${mod}+Semicolon" = "exec ${pkgs.swaylock-effects}/bin/swaylock";
         "XF86AudioMute" = "exec ${wpctl} set-mute @DEFAULT_AUDIO_SINK@ toggle";
         "XF86AudioLowerVolume" = "exec ${wpctl} set-volume @DEFAULT_AUDIO_SINK@ 5%-";
-        "XF86AudioRaiseVolume" = "exec ${wpctl}/bin/wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%+";
+        "XF86AudioRaiseVolume" = "exec ${wpctl} set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%+";
         "XF86AudioPrev" = "exec ${playerctl} previous";
         "XF86AudioPlay" = "exec ${playerctl} play-pause";
         "XF86AudioNext" = "exec ${playerctl} next";
