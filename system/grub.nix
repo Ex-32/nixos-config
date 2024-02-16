6205159d222b449b593771b5273ea05747781c10
Ex-32 <jenna@fligor.net>
2024-02-16 14:44:16 -0600

    clean up custom grub theme derivation

--- system/grub.nix
+++ system/grub.nix
@@ -5,7 +5,9 @@
     canTouchEfiVariables = true;
     efiSysMountPoint = "/boot/efi";
   };
-  boot.loader.grub = {
+  boot.loader.grub = let
+    font-path = "${pkgs.spleen}/share/fonts/misc/spleen-16x32.otf";
+  in {
     enable = true;
     device = "nodev";
     efiSupport = true;
@@ -16,11 +18,11 @@
     # i'm honestly not sure if this is doing anything since i defined a custom
     # theme, but since the spleen packages is being used in that theme anyways,
     # it's not pulling in any additional packages, so if it ain't broke...
-    font = "${pkgs.spleen}/share/fonts/misc/spleen-16x32.otf";
+    font = "${font-path}";
     fontSize = 32;
 
     theme = let
-      path = "src/catppuccin-mocha-grub-theme";
+      theme-path = "src/catppuccin-mocha-grub-theme";
     in pkgs.stdenv.mkDerivation rec {
       pname = "catppuccin-mocha-grub";
       version = "1.0.0";
@@ -37,15 +39,16 @@
       # HACK: using find 'n replace to change the font and to use relative
       # sizes for the menu options (the original didn't scale to 4k very well)
       prePatch = ''
-        substituteInPlace ${path}/theme.txt --replace "Unifont Regular 16" "Spleen 16x32 Regular 32"
-        substituteInPlace  ${path}/theme.txt --replace "left = 50%-240" "left = 20%"
-        substituteInPlace ${path}/theme.txt --replace "width = 480" "width = 60%"
+        substituteInPlace ${theme-path}/theme.txt \
+          --replace "Unifont Regular 16" "Spleen 16x32 Regular 32" \
+          --replace "left = 50%-240" "left = 20%" \
+          --replace "width = 480" "width = 60%"
       '';
       buildPhase = ''
-        grub-mkfont --size 32 ${pkgs.spleen}/share/fonts/misc/spleen-16x32.otf -o ${path}/font.pf2
+        grub-mkfont --size 32 ${font-path} -o ${theme-path}/font.pf2
       '';
       installPhase = ''
-        cp -r ${path} $out
+        cp -r ${theme-path} $out
       '';
     };
   };
