1c15f64097af2d960ed1a3a2f85bb207c86c31f8
Ex-32 <jenna@fligor.net>
2024-02-28 00:18:11 -0600

    enable systray applets based on if the service they connect to is enabled

--- user/systray.nix
+++ user/systray.nix
@@ -1,9 +1,10 @@
-{ config, pkgs, lib, inputs, ... }:
+{ config, pkgs, lib, inputs, osConfig, ... }:
 
 {
   services.udiskie = {
     enable = true;
     automount = false;
   };
-  services.network-manager-applet.enable = true;
+  services.network-manager-applet.enable = osConfig.networking.networkmanager.enable;
+  services.blueman-applet.enable = osConfig.services.blueman.enable;
 }
