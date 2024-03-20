fdb0625906b296cdd1274918bcc9efa3a795d7e4
Jenna Fligor <jenna@fligor.net>
2024-03-20 12:30:01 -0500

    set fish variables for vi mode cursor shape

--- user/fish.nix
+++ user/fish.nix
@@ -29,6 +29,9 @@
       */
       ''
         fish_vi_key_bindings
+        set -g fish_cursor_insert line
+        set -g fish_cursor_replace_one underscore
+        set -g fish_cursor_visual block
       '';
     functions = {
       leak.body =
