834fa9ce1624c49aab16b3dd8f3bb1212557ce9d
Jenna Fligor <jenna@fligor.net>
2024-03-28 01:12:14 -0500

    switched nixbook to suspend on short power button press

--- hardware/nixbook.nix
+++ hardware/nixbook.nix
@@ -122,6 +122,10 @@
       MEM_SLEEP_ON_BAT = "deep";
     };
   };
+  services.logind.extraConfig = ''
+    # short power-button press suspends the laptop rather than powering it off
+    HandlePowerKey=suspend
+  '';
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
