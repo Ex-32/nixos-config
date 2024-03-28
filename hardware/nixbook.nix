78145a28ebe657f239eda0cfbf9528b0ea472b96
Jenna Fligor <jenna@fligor.net>
2024-03-28 01:12:45 -0500

    added more stringent TLP settings to nixbook

--- hardware/nixbook.nix
+++ hardware/nixbook.nix
@@ -120,6 +120,20 @@
     settings = {
       MEM_SLEEP_ON_AC = "deep";
       MEM_SLEEP_ON_BAT = "deep";
+
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+      CPU_MIN_PERF_ON_AC = 0;
+      CPU_MAX_PERF_ON_AC = 100;
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_MAX_PERF_ON_BAT = 30;
+
+      CPU_BOOST_ON_AC = 1;
+      CPU_BOOST_ON_BAT = 0;
     };
   };
   services.logind.extraConfig = ''
