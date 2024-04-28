7bb35c54cc6390a878aac1ac4876866d41faafd1
Jenna Fligor <jenna@fligor.net>
2024-04-27 20:01:20 -0500

    condensed sway config

--- home-manager/swayidle.nix
+++ home-manager/swayidle.nix
@@ -5,19 +5,4 @@
   inputs,
   ...
 }: {
-  services.swayidle = {
-    enable = true;
-    timeouts = [
-      {
-        timeout = 180;
-        command = "${pkgs.swaylock-effects}/bin/swaylock --grace 20";
-      }
-    ];
-    events = [
-      {
-        event = "before-sleep";
-        command = "${pkgs.swaylock-effects}/bin/swaylock";
-      }
-    ];
-  };
 }
