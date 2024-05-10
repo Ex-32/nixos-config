7253eb27f9e1fcab391ca81231df0b1ef4f55981
Jenna Fligor <jenna@fligor.net>
2024-05-10 11:18:18 -0500

    fixed deprecation warnings

--- home-manager/qt.nix
+++ home-manager/qt.nix
@@ -14,7 +14,7 @@
 
   qt = {
     enable = true;
-    platformTheme = "gnome";
+    platformTheme.name = "adwaita";
     style = {
       name = "kvantum-dark";
       package = pkgs.libsForQt5.qtstyleplugin-kvantum;
