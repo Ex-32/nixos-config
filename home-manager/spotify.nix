9603696e573dbcf84a7eafde1c2bc86e3e07ee76
Jenna Fligor <jenna@fligor.net>
2024-07-09 11:00:26 -0500

    override spicetify catppuccin package source with newer version

--- home-manager/spotify.nix
+++ home-manager/spotify.nix
@@ -13,7 +13,14 @@ in {
 
   programs.spicetify = {
     enable = true;
-    theme = spicePkgs.themes.catppuccin;
+    theme = spicePkgs.themes.catppuccin // {
+      src = pkgs.fetchFromGitHub {
+        owner = "catppuccin";
+        repo = "spicetify";
+        rev = "4294a61f54a044768c6e9db20e83c5b74da71091";
+        hash = "sha256-OHsauoCjj99aoIbq78xQf1ehYtLpIcUde5DmZSJFCXI=";
+      };
+    };
     colorScheme = "mocha";
 
     enabledExtensions = with spicePkgs.extensions; [
