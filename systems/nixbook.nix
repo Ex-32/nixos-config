1cbdbdaf9b09619a53035293f7a6fe74b22181a3
Jenna Fligor <jenna@fligor.net>
2024-07-14 16:53:03 -0500

    make power button power again

--- systems/nixbook.nix
+++ systems/nixbook.nix
@@ -115,10 +115,6 @@ in {
       CPU_BOOST_ON_BAT = 0;
     };
   };
-  services.logind.extraConfig = ''
-    # short power-button press suspends the laptop rather than powering it off
-    HandlePowerKey=suspend
-  '';
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
