bed630fbb1092d5aa4eb7d16704534c16caac2fe
Jenna Fligor <jenna@fligor.net>
2024-10-08 11:15:04 -0500

    neovim: removed some C/C++ conceals

--- config/nvim/after/queries/c/highlights.scm
+++ config/nvim/after/queries/c/highlights.scm
@@ -2,14 +2,14 @@
 
 ;; types
 (((_ (primitive_type) @type) (#eq? @type "int")) (#set! conceal "ℤ"))
-(((_ (primitive_type) @type) (#eq? @type "void")) (#set! conceal "🛇"))
+; (((_ (primitive_type) @type) (#eq? @type "void")) (#set! conceal "🛇"))
 (((_ (primitive_type) @type) (#eq? @type "bool")) (#set! conceal "𝔹"))
 
 ;; qualifiers
 ((sized_type_specifier ("unsigned") @type) (#offset! 0 0 0 2) (#set! conceal "∓̸"))
 (((_ (type_qualifier) @keyword) (#eq? @keyword "const")) (#set! conceal "C"))
 (((_ (storage_class_specifier) @keyword) (#eq? @keyword "static")) (#set! conceal "S"))
-((struct_specifier ("struct") @type) (#set! conceal " "))
+; ((struct_specifier ("struct") @type) (#set! conceal " "))
 
 ;; primitives
 ((unary_expression ("!") @operator) (#set! conceal "¬"))
