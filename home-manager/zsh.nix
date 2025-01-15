9c78b285b196d1acdd87affc4bddbd7fe6ede76e
Jenna Fligor <jenna@fligor.net>
2025-01-14 20:23:29 -0600

    removed p10k

--- home-manager/zsh.nix
+++ home-manager/zsh.nix
@@ -4,15 +4,9 @@
   lib,
   inputs,
   ...
-}: let
-  p10k-path = ".config/zsh/.p10k.zsh";
-in {
-  home.file."${p10k-path}".source = ../config/zsh/p10k.zsh;
-
+}: {
   programs = {
-    zsh = let
-      p10k-source = ''[ "$TERM" = 'linux' ] || source'';
-    in {
+    zsh = {
       enable = true;
       autocd = true;
       dotDir = ".config/zsh";
@@ -29,30 +23,27 @@ in {
         size = ZLONG_MAX;
       };
 
-      initExtraFirst = ''
-        # make ctrl+arrow-key work as expected
-        # (for kitty at least, may vary with terminal emulators)
-        bindkey "^[[1;5C" forward-word
-        bindkey "^[[1;5D" backward-word
+      initExtra =
+        # sh
+        ''
+          # make ctrl+arrow-key work as expected
+          # (for kitty at least, may vary with terminal emulators)
+          bindkey "^[[1;5C" forward-word
+          bindkey "^[[1;5D" backward-word
 
-        ${p10k-source} ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
-      '';
-      initExtra = ''
-        ns() {
-            nix_shell=pure
-            for arg in $@ ; do
-                [[ "$arg" == "--impure" ]] && nix_shell=impure
-                if [[ "$arg" =~ "--.*" ]] ; then
-                    arg_list="$arg_list '$arg'"
-                else
-                    arg_list="$arg_list 'nixpkgs#$arg'"
-                fi
-            done
-            eval "env IN_NIX_SHELL=$nix_shell nix shell $arg_list"
-        }
-
-        ${p10k-source} ~/${p10k-path}
-      '';
+          ns() {
+              nix_shell=pure
+              for arg in $@ ; do
+                  [[ "$arg" == "--impure" ]] && nix_shell=impure
+                  if [[ "$arg" =~ "--.*" ]] ; then
+                      arg_list="$arg_list '$arg'"
+                  else
+                      arg_list="$arg_list 'nixpkgs#$arg'"
+                  fi
+              done
+              eval "env IN_NIX_SHELL=$nix_shell nix shell $arg_list"
+          }
+        '';
     };
     direnv = {
       enable = true;
