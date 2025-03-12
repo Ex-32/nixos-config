74ac5f7265cdd24202fbba48befbb438324dde63
Jenna Fligor <jenna@fligor.net>
2025-03-12 11:46:03 -0500

    added slack and ms teams to social apps

--- home-manager/socials.nix
+++ home-manager/socials.nix
@@ -8,14 +8,17 @@
   allowedUnfree = [
     "discord"
     "signal-desktop"
+    "slack"
   ];
 
   home.packages = with pkgs;
     [
       discord
       signal-desktop
+      slack
     ]
     ++ (lib.optionals (builtins.elem pkgs.system lib.platforms.linux) (with pkgs; [
       nheko
+      teams-for-linux
     ]));
 }
