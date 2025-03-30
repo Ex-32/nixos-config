566833876925163e74ae52a18743bc0a1967e06f
Jenna Fligor <jenna@fligor.net>
2025-03-30 13:40:57 -0500

    removed zion non-standard ssh port

--- nixos/ssh.nix
+++ nixos/ssh.nix
@@ -9,11 +9,6 @@
     enable = true;
     startWhenNeeded = true;
 
-    # given the number of automated port 22 spamming bots out there, i prefer
-    # to keep my ssh on another port, i don't remember why i originally chose
-    # this one, but it's my default go-to for ssh now
-    ports = [3932];
-
     # i'm dumb not stupid
     settings.PermitRootLogin = "no";
   };
